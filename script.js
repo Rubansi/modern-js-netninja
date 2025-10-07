a8511ee3804ff4dfbfe8ab3fb564ce9d5be01ba4
rubansi <rubansi24@gmail.com>
2025-10-07 17:52:19 +0300

    logging to the browser console

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 alert("hello world");
+
+// Note: double quotes and single quotes are both valid in JavaScript
+// but it's best to be consistent and stick to one style
+alert('hello world'); 
+
+// logging to the console (good for debugging)
 console.log("hello world, welcome to modern javascript");
