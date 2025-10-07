7d15cef3c8a07d05fe7f3e9297c010aa6ffdcbfc
rubansi <rubansi24@gmail.com>
2025-10-07 19:33:42 +0300

    exploring strings

--- strings.js
+++ strings.js
@@ -12,7 +12,7 @@ console.log(fullName1); // output: JohnDoe
 
 //template strings (template literals)
 let age = 30;
-let greeting = `Hello, my name is ${firstName} ${lastName} and I am ${age} years old.`;
+let greeting = `Hello, my name is ${firstName} ${lastName} and I am ${age} years old.`; // using backticks `` and ${} for variables
 console.log(greeting); // output: Hello, my name is John Doe and I am 30 years old.
 
 
