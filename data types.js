57b0612fc064f51f7914d836ff8f984bacfb2ded
rubansi <rubansi24@gmail.com>
2025-10-07 18:01:52 +0300

    data types - strings

---
+++ data types.js
@@ -0,0 +1,6 @@
+//there are several data types in javascript
+//1. string - text wrapped in single or double quotes
+let name = "John"; // or let name = 'John';
+console.log(name);  // output: John
+
+       
