0791849eb7ad9362d98fc5521843a3a2dae196b2
rubansi <rubansi24@gmail.com>
2025-10-07 17:58:33 +0300

    variables and constants

--- variables and constants.js
+++ variables and constants.js
@@ -9,3 +9,9 @@ console.log(age);
 
 var score = 75; // 'var' is the old way of declaring variables, avoid using it in modern JS
 console.log(score); // still works, but not recommended
+
+// Variables Naming conventions
+// 1.cannot start with a number (e.g., let 1stName = 'John'; is invalid)
+// 2.cannot contain spaces (e.g., let first name = 'John'; is invalid)
+// 3. can use letters, numbers, underscores (_), and dollar signs ($)
+// 4.camelCase is the common convention for naming variables in javascript (e.g., let firstName = 'John';)
