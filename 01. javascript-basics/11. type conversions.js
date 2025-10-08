88baebff6b657b5e9768fc4c1dc3fd3a256f77f9
rubansi <rubansi24@gmail.com>
2025-10-08 10:47:12 +0300

    exploring type conversions

--- 01. javascript-basics/11. type conversions.js
+++ 01. javascript-basics/11. type conversions.js
@@ -15,3 +15,15 @@ let num1 = 100;
 let num2 = 99.99;
 console.log(String(num1)); // output: "100" (number to string)
 console.log(String(num2)); // output: "99.99" (number to string)    
+
+//converting booleans to strings
+let bool1 = true;
+let bool2 = false;
+console.log(String(bool1)); // output: "true" (boolean to string)
+console.log(String(bool2)); // output: "false" (boolean to string)
+
+//converting strings to booleans
+let strTrue = "true";
+let strFalse = "false";
+let strEmpty = "";
+console.log(Boolean(strTrue));
