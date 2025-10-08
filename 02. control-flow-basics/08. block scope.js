2979848f3905118f37e7161430be7165e3f6f0a8
rubansi <rubansi24@gmail.com>
2025-10-08 18:05:49 +0300

    variables and block scope

--- 02. control-flow-basics/08. block scope.js
+++ 02. control-flow-basics/08. block scope.js
@@ -1,10 +1,15 @@
 // variables and block scope
 
-let age = 50; //global scope outside block
+let age = 30; //global scope outside block
 
 if(true){
    let age = 40; //local scope, valid inside the code block
     console.log('inside 1st code block: ', age);
+
+    if(true){
+        let age = 50;
+         console.log('inside 2nd code block: ', age);
+    }
 }
 
 console.log('outside code block: ', age);
