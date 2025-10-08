d3fa334d2c8af77bffda6e931a6f178bae159099
rubansi <rubansi24@gmail.com>
2025-10-08 15:26:25 +0300

    break and continue

--- 02. control-flow-basics/06. break and continue.js
+++ 02. control-flow-basics/06. break and continue.js
@@ -5,6 +5,15 @@ for(let i = 0; i< scores.length; i++){
 
     if (scores[i] === 100){
         console.log('congrats, you got the top score!')
-        break;
+        break; //breaks out of the loop, no more iterations from this point.
     }
+}
+
+//continue keyword
+
+for(let i = 0; i< scores.length; i++){
+        if (scores[i] === 0){
+        continue;        
+    }
+    console.log('your score: ', scores[i]);
 }
