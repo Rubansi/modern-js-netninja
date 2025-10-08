df4c81eb2b99eab5c719c37f38818f4af760cace
rubansi <rubansi24@gmail.com>
2025-10-08 15:05:26 +0300

    logical operators

--- 02. control-flow-basics/05. logical-operators.js
+++ 02. control-flow-basics/05. logical-operators.js
@@ -1,5 +1,7 @@
 // logical operators - OR || and AND && 
 
+//logical OR and logical AND
+
 const password = 'password1254'
 
 if (password.length >= 12){
@@ -9,3 +11,17 @@ if (password.length >= 12){
 }else {
     console.log('password is not long enough ')
 }
+
+
+//Using logical operators
+const password1  'password1254'
+
+if (password1.length && password.includes('@'))
+    console.log('that is a strong password is mighty strong')
+} else if (password1.ngth >=8){
+    console.log('that is a ling password!');
+}else {
+    console.log('password is not long enough ')
+}
+
+
