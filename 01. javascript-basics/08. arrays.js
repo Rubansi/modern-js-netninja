d4bb47805eaaf69cf137077e3dbdbcaa38131b47
rubansi <rubansi24@gmail.com>
2025-10-08 09:59:51 +0300

    exploring arrays

---
+++ 01. javascript-basics/08. arrays.js
@@ -0,0 +1,10 @@
+// an array of fruits   
+let fruits = ["Apple", "Banana", "Cherry"];
+console.log(fruits); // output: [ 'Apple', 'Banana', 'Cherry' ]
+
+// accessing elements in an array
+console.log(fruits[0]); // output: Apple
+console.log(fruits[1]); // output: Banana
+console.log(fruits[2]); // output: Cherry
+
+// adding an element to the end of the array
