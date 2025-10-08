d5e7ebd76b1821b9c7785cf807b8b38e37d7f83c
rubansi <rubansi24@gmail.com>
2025-10-08 12:16:46 +0300

    control flows - if statemennts

---
+++ 02. control-flow-basics/02. while loop.js
@@ -0,0 +1,19 @@
+// while loops
+
+//example 1 
+let i = 0;
+
+while (i < 5){
+      console.log('in loop:', i);
+      i++; // this helps to avoid an infinite loop. // otherwise without it this will execute an infinite loop 
+}
+
+// example 2
+const names = ['rubansi', 'vincent', 'nyauma']
+let i = 0;
+while (i<names.length){
+    console.log (names[i]);
+    i++;
+}
+
+//a while loop is an alternative to a for loop
---
+++ 02. control-flow-basics/04. if statements.js
@@ -0,0 +1,32 @@
+// if statements - conditional statements
+
+// we execute the code when the condition is true
+
+
+//example 1
+const age = 20;
+
+if (age > 20){
+    console.log('you are over 20 years old');
+    //this code executes when the condition is true
+}
+
+//example 2
+const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi'];
+
+if (ninjas.length > 4){
+    console.log("that is a lot of ninjas");
+}
+
+
+// the esle and else if statement
+const password = 'password123';
+if (password.length >= 8){
+    console.log('password is long enough');
+} else {
+    console.log('password is not long enough')
+}
+
+
+
+
