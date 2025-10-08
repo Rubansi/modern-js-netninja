7fc7e26b5d44d69c9db96bcd79891ba052029a4f
rubansi <rubansi24@gmail.com>
2025-10-08 11:19:39 +0300

    control flows - for loops

--- 02. control-flow-basics/01. for loops.js
+++ 02. control-flow-basics/01. for loops.js
@@ -12,5 +12,5 @@ const names = ['shaun', 'mario', 'luigi'];
 
 for (let i=0; i<names.length; i++){
     console.log(names[i]);
-    let html = `<div> &{names[i]} </div>`
+    let html = `<div> &{names[i]} </div>`;
 }
