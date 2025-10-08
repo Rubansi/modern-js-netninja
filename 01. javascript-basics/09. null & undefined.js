f834ef29dbb2fdf63270b3e52129b580676e4ac8
rubansi <rubansi24@gmail.com>
2025-10-08 10:25:04 +0300

    null and undefined

---
+++ 01. javascript-basics/09. null & undefined.js
@@ -0,0 +1,18 @@
+
+
+let age;
+console.log(age); // output: undefined
+//undefined means a variable has been declared but not assigned a value
+
+//difference between null and undefined
+let data = null; // explicitly set to no value
+console.log(data); // output: null  
+let info; // declared but not assigned
+console.log(info); // output: undefined
+
+//undefined is the default value for uninitialized variables
+let score;
+console.log(score); // output: undefined    
+//null is used to indicate the absence of a value
+let result = null;
+console.log(result); // output: null
