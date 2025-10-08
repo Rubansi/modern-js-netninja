edd5cfb9d533282e6993687d93ce7a1191127d39
rubansi <rubansi24@gmail.com>
2025-10-08 17:41:49 +0300

    switch statement

--- 02. control-flow-basics/07. switch statement.js
+++ 02. control-flow-basics/07. switch statement.js
@@ -7,6 +7,17 @@ const grade = 'A';
 if (grade === 'A'){
     console.log ('student scored A')
 } else if (grade === 'B' ){
-
+    console.log ('student scored B')
+    
+} else if (grade === 'C' ){
+    console.log ('student scored C')
+    
+} else if (grade === 'D' ){
+    console.log ('student scored D')
+    
+} else if (grade === 'E' ){
+    console.log ('student scored E')
+    
+} else {
 
 }
