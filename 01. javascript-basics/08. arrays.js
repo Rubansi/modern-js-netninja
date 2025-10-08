5b0e3da14161d24392e869b5b8a52d9955ad871f
rubansi <rubansi24@gmail.com>
2025-10-08 10:07:40 +0300

    exploring arrays

--- 01. javascript-basics/08. arrays.js
+++ 01. javascript-basics/08. arrays.js
@@ -7,4 +7,11 @@ console.log(fruits[0]); // output: Apple
 console.log(fruits[1]); // output: Banana
 console.log(fruits[2]); // output: Cherry
 
-// adding an element to the end of the array
+// adding an element to the end of the array    
+fruits.push("Date");
+console.log(fruits); // output: [ 'Apple', 'Banana', 'Cherry', 'Date' ]
+
+// removing the last element from the array
+let lastFruit = fruits.pop();
+console.log(lastFruit); // output: Date
+console.log(fruits); // output: [ 'Apple', 'Banana', 'Cherry' ]
