b661f14150b5a8345dac9defda998980fb87fc8a
rubansi <rubansi24@gmail.com>
2025-10-08 17:48:12 +0300

    switch statement

--- 02. control-flow-basics/07. switch statement.js
+++ 02. control-flow-basics/07. switch statement.js
@@ -21,3 +21,23 @@ if (grade === 'A'){
 } else {
     console.log ('not a valid grade')
 }
+
+
+//the switch statement
+
+const marks = 'D';
+
+switch(marks){
+    case 'A':
+        console.log('you got an A!');
+    case 'B':
+        console.log('you got a B!');
+    case 'C':
+        console.log('you got a C!');
+    case 'D':
+        console.log('you got a D!');
+    case 'E':
+        console.log('you got an A!');
+    default:
+        console.log('not a valid grade');
+}
