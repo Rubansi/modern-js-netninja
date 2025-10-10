e3df22eaab229cb945b894dab0f2de7ebd091189
rubansi <rubansi24@gmail.com>
2025-10-10 11:58:50 +0300

    callback functions

--- 03. functions and methods.js/06. callback functions & foreach.js
+++ 03. functions and methods.js/06. callback functions & foreach.js
@@ -16,3 +16,11 @@ myFunc(value => { // parsing a function as an argument
     //do something
     console.log(value);
 });
+
+
+//example
+let people = ['mario', 'luigi', 'ryu', 'shaun', 'china'];
+people.forEach(function(){
+    console.log('something') //functions fires up five times with the string 'something' for each individual element
+});
+
