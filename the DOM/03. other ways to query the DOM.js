7606b10c969cff5e8adb9dc96af73f8a9dd78793
rubansi <rubansi24@gmail.com>
2025-10-10 20:16:37 +0300

    other ways of querying the DOM

--- the DOM/03. other ways to query the DOM.js
+++ the DOM/03. other ways to query the DOM.js
@@ -5,8 +5,8 @@ const title = document.getElementBy('page.title');
 console.log(title);
 
 // get elements by their class name
-
-
+const errors = document.getElementsByClassName('error')  // no need to use dots
+console.log(errors) // outputs an html collection and not a node list, we cannot use the forEach function to iterate in the html collection
 
 
 // get elements by their tag name
