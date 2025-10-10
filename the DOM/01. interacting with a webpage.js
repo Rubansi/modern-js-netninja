d39fb38a48019aab1ee732bc232e4bd5fa60e653
rubansi <rubansi24@gmail.com>
2025-10-10 19:23:52 +0300

    the document object model - DOM

--- the DOM/01. interacting with a webpage.js
+++ the DOM/01. interacting with a webpage.js
@@ -1,2 +1,2 @@
 // the DOM is used to interact with html doc using javascript
-// the DOM is ideally a document object created by the browser to model the html page - so its an object called document
+// the DOM is ideally a document object created by the browser to model the html page - so its an object called document i.e. the document object
