f10aa9f2e899bf8998bff78787eb3cf09914476d
rubansi <rubansi24@gmail.com>
2025-10-10 19:05:24 +0300

    primitive types and reference types

--- 04. object literals/06. primitive types and reference types.js
+++ 04. object literals/06. primitive types and reference types.js
@@ -5,3 +5,14 @@
 //stack and heap - different parts of memory
 // stack - when a primitive types is created its stored in a stack, space is limited.
 // heap - reference types are stored in the heap, adequate space is available, a pointer is set to the stack when a reference types is created
+
+//important: when we make a copy of a primitive types, we make a duplicate of the values 
+// but when we make a copy of a reference types we only make a copy of the pointer but the values remain the same
+
+
+// primitive values
+
+let scoreOne = 50;
+let scoreTwo = scoreOne;
+
+console.log(`scoreOne: ${scoreOne}`, `scoreTwo: ${scoreTwo} );
