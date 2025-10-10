354f15bed552ebd32ed32130a2512d5b92b697e7
rubansi <rubansi24@gmail.com>
2025-10-10 19:00:28 +0300

    primitive types and reference types

--- 04. object literals/06. primitive types and reference types.js
+++ 04. object literals/06. primitive types and reference types.js
@@ -1,3 +1,7 @@
 // primitive types - numbers, strings, booleans, null, symbols
 // reference types - all types of objects, object literals, arrays, functions, dates, all other objects
 
+
+//stack and heap - different parts of memory
+// stack - when a primitive types is created its stored in a stack, space is limited.
+// heap - reference types are stored in the heap, adequate space is available, a pointer is set to the stack when a reference types is created
