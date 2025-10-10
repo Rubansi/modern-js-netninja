0e63a3e61e127ce63c4747f31bfd866947382bb4
rubansi <rubansi24@gmail.com>
2025-10-10 20:37:21 +0300

    adding or changing text

--- the DOM/04. adding & changing page content.js
+++ the DOM/04. adding & changing page content.js
@@ -13,8 +13,11 @@ paras.forEach(para => {
     para.innerText += ' new extra text' // adding extra text
 });
 
-
+// adding content into html
 const content = document.querySelector('.content');
 // console.log(content.innerHTML);
 
 content.innerHTML = '<h2>THIS IS A NEW HEADING</h2>'; // completely overrides the content, use += to append text
+
+
+
