2cb7c2e96fc65bf1cf671876d589980963a85441
rubansi <rubansi24@gmail.com>
2025-10-10 23:23:16 +0300

    getting and setting attributes

--- the DOM/05. getting and setting attributes.js
+++ the DOM/05. getting and setting attributes.js
@@ -1,10 +1,13 @@
-//
+// getting and setting values of an href attribute
 const link = document.querySelector('a');
 
 console.log(link.getAttribute('href'));
 link.setAttribute('href', 'https://www.thenetninja.co.uk');
 link.innerText =  'the net ninja website';
 
+
+// getting and setting the value of a paragraph
 const message = document.querySelector('p');
 
-console.log(message);
+console.log(message.getAttribute('class'));
+message.setAttribute('class', 'success')
