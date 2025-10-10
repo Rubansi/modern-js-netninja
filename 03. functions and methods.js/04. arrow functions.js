331edcce33903e299bcf5b0474e0d0cc15e46c0a
rubansi <rubansi24@gmail.com>
2025-10-10 11:04:04 +0300

    arrow functions

--- 03. functions and methods.js/04. arrow functions.js
+++ 03. functions and methods.js/04. arrow functions.js
@@ -1,12 +1,16 @@
 //regular function
-const calcArea = functions(radius){
+const calcArea1 = function (radius){
     return 3.14 * radius**2;
 
 }
 
+const area1 = calcArea1(5);
+console.log('area is: ', area1);
+
 //arrow function
-
-
+const calcArea = (radius) =>  {
+    return 3.14 * radius**2;
+}
 
 const area = calcArea(5);
 console.log('area is: ', area);
