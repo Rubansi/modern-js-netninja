accd7356a628386935fdb810bb0a8ce834a4344e
rubansi <rubansi24@gmail.com>
2025-10-10 11:18:24 +0300

    arrow functions

--- 03. functions and methods.js/04. arrow functions.js
+++ 03. functions and methods.js/04. arrow functions.js
@@ -9,10 +9,15 @@ console.log('area is: ', area1);
 
 
 
-//arrow function, newer and shorter way to write functions
+//arrow function, newer and shorter way to write functions, with one parameter we do not need parenthesis
 const calcArea = (radius) =>  {
     return 3.14 * radius**2;
 }
 
 const area = calcArea(5);
 console.log('area is: ', area);
+
+//arrow functions with one parameter and one return value
+const area2 = calcArea2 = radius => 3.14 * radius **2;
+
+
