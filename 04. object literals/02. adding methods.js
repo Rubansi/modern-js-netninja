147e433fe3c101d27d2905724910b3040aea0c01
rubansi <rubansi24@gmail.com>
2025-10-10 18:25:35 +0300

    adding methods

---
+++ 04. object literals/02. adding methods.js
@@ -0,0 +1,18 @@
+// object literals
+
+let user = {
+    name: 'Crystal',
+    age: 30,
+    email: 'crystal@example.com',
+    location: 'berlin',
+    blogs: ['the river between', 'a man of the people', 'the enemy of the people'],
+    login: function (){
+        console.log('the use is logged in');
+    }
+};
+
+user.login();
+
+const name = 'mario';
+name.toUpperCase();
+
