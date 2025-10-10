1d8492cb818455a7dbb091eac8a0b5bde40a4405
rubansi <rubansi24@gmail.com>
2025-10-10 12:11:30 +0300

    callback functions

--- 03. functions and methods.js/06. callback functions & foreach.js
+++ 03. functions and methods.js/06. callback functions & foreach.js
@@ -24,3 +24,25 @@ people.forEach(function(){
     console.log('something') //functions fires up five times with the string 'something' for each individual element
 });
 
+let persons = ['mario', 'luigi', 'ryu', 'shaun', 'china'];
+persons.forEach(function(persons){
+    console.log(persons) //functions fires up five times with the string 'something' for each individual element
+});
+
+//converted to arrow function
+let personList = ['mario', 'luigi', 'ryu', 'shaun', 'china'];
+personList.forEach((person, index) => {
+    console.log(person) //functions fires up five times with the string 'something' for each individual element
+});
+
+
+//defining a callback function the parsing it
+let country = ['Kenya', 'Uganda', 'Tanga', 'Rwanda', 'Malawi'];
+
+const logCountry = (person, index) => {
+    console.log(`${index} - hello ${country}`);
+}
+
+country.forEach((country, index) => {
+    console.log(index, country)
+});
