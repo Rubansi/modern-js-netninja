beadeee8f5350f80dbe58ac07960425119710e6a
rubansi <rubansi24@gmail.com>
2025-10-10 19:27:06 +0300

    the document object model - DOM

--- the DOM/01. interacting with a webpage.js
+++ the DOM/01. interacting with a webpage.js
@@ -3,3 +3,7 @@
 
 // since its an objects it has various methods. these are inbuilt methods that we can use out of the box
 
+// the DOM also describes how the html looks visually, the root is html tag, then head and body tag etc - its like an hirachical tree of nodes
+
+// so we use the tree of nodes access them and manipulate the html document using javascript using certain methods
+
