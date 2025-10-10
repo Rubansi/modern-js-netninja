73584710859b509a42a1623b3702d7e715b4b3a9
rubansi <rubansi24@gmail.com>
2025-10-10 23:25:42 +0300

    getting and setting attributes

--- the DOM/05. getting and setting attributes.js
+++ the DOM/05. getting and setting attributes.js
@@ -1,4 +1,4 @@
-// getting and setting values of an href attribute
+// example 1 - getting and setting values of an href attribute
 const link = document.querySelector('a');
 
 console.log(link.getAttribute('href'));
@@ -6,7 +6,7 @@ link.setAttribute('href', 'https://www.thenetninja.co.uk');
 link.innerText =  'the net ninja website';
 
 
-// getting and setting the value of a paragraph
+// example 2 - getting and setting the value of a paragraph
 const message = document.querySelector('p');
 
 console.log(message.getAttribute('class'));
