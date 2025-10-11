f2dba7ce0a1d29233077e0edfa9af37e4a794105
rubansi <rubansi24@gmail.com>
2025-10-11 12:40:54 +0300

    events and event listeners

--- the DOM/events and event listeners/app.js
+++ the DOM/events and event listeners/app.js
@@ -1,7 +1,7 @@
 //removing elements from the DOM
 
-// const ul = document.querySelector('ul');
-// ul.remove(); 
+const ul = document.querySelector('ul');
+ul.remove(); 
 
 
 // adding an event listener
@@ -14,13 +14,13 @@ const items = document.querySelectorAll('li');
 
 items.forEach(item => {
     item.addEventListener('click', e => {
-        // console.log('item clicked');
-        // console.log(e);
-        // console.log(e.target); //use this often
-        // console.log(item);
+        console.log('item clicked');
+        console.log(e);
+        console.log(e.target); //use this often
+        console.log(item);
         e.target.style.textDecoration = 'line-through';
 
-        // e.target.remove();
+        e.target.remove();
 
     })
 });
