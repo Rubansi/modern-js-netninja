f80b851c5687039001101d8b5d4f052ea2f0497a
rubansi <rubansi24@gmail.com>
2025-10-11 10:48:48 +0300

    adding and removing classes

--- the DOM/07. adding and removing classes.js
+++ the DOM/07. adding and removing classes.js
@@ -1,3 +1,10 @@
+
+
 const content = document.querySelector('p');
 
-console.log(content.classList);
+console.log(content.classList); // accessing classes in a document
+
+// adding classes
+content.classList.add('nice');
+content.classList.remove('nice');
+content.classList.add('rice');
