04d3b589bd7b1260a0e2a1ac2b9933b12d88d9bd
rubansi <rubansi24@gmail.com>
2025-10-11 10:31:33 +0300

    getting and setting attributes

--- the DOM/06. adding and removing styles.js
+++ the DOM/06. adding and removing styles.js
@@ -1,8 +1,11 @@
 // adding and removing styles
 
-const title = document.querySelector('h1');
+const title = document.querySelector('h1');  
 
-// title.setAttribute('style', 'margin: 50px;');
+// title.setAttribute('style', 'margin: 50px;'); // overrides the inline css style in html
 
 console.log(title.style);
 console.log(title.style.color);
+
+title.style.margin = '50px';
+title.style.color = 'crimson'
