3a276417fdd7a234956168b024f3d1d5025fd3bf
rubansi <rubansi24@gmail.com>
2025-10-11 18:46:09 +0300

    making a popup

--- the DOM/making a pop up/popup.js
+++ the DOM/making a pop up/popup.js
@@ -10,7 +10,7 @@ close.addEventListener('click', () => {
     popup.style.display = 'none';
 });
 
-close.addEventListener('click', () => {
+popup.addEventListener('click', () => {
     popup.style.display = 'none';
 });
 
