8f3f1544d97114f2f0ccc1636b47e062148d73a3
rubansi <rubansi24@gmail.com>
2025-10-11 12:55:29 +0300

    events and event listeners

--- the DOM/adding elements/app.js
+++ the DOM/adding elements/app.js
@@ -6,8 +6,8 @@ const button = document.querySelector('button');
 button.addEventListener('click', () => {
     const li = document.createElement('li');
     li.textContent = 'something new to do';
-    // ul.append(li);
-    ul.prepend(li);
+    // ul.append(li); // ads the element to the end of the list
+    ul.prepend(li); // adds the item to the start of the list
 });
 
 
