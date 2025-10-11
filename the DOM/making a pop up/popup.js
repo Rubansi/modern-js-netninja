2f7215b9a7dbec2b7dd44c8d9cfe87fb75e28f1a
rubansi <rubansi24@gmail.com>
2025-10-11 18:49:04 +0300

    making a popup

--- the DOM/making a pop up/popup.js
+++ the DOM/making a pop up/popup.js
@@ -1,5 +1,6 @@
 const button = document.querySelector('button');
 const popup = document.querySelector('.popup-wrapper');
+const close = document.querySelector('popup-close');
 
 button.addEventListener('click', () => {
     popup.style.display = 'block';
